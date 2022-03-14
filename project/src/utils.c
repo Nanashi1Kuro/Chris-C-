2e20156bcc7e15a972e71d41b01c807d21612723
Kristina Khakimova <khakimovakm@gmail.com>
2022-03-14 17:06:43 +0300

    1 done

--- project/src/utils.c
+++ project/src/utils.c
@@ -6,13 +6,17 @@
 
 size_t timer_from(int from) {
     size_t counter = 0;
-    for (int i = from; i > 0; --i) {
-        ++counter;
-        printf("%i ",i);
+    if(from > 0){
+        for (int i = from; i > 0; --i) {
+            ++counter;
+            printf("%i ",i);
+        }
+        printf("0");
+        printf("\n");
+        return ++counter;
     }
-    printf("0");
-    printf("\n");
-    return ++counter;
+    else
+    return counter;
 }
 
 // TODO: Implement `power of` function
